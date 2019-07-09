50958a57b64e22a340db40c801239d471de29d09
zhangyy999 <1974423726@qq.com>
2019-07-09 22:07:56 +0800

    修改user实体类

--- knowstars_common/src/main/java/com/jk/pojo/User.java
+++ knowstars_common/src/main/java/com/jk/pojo/User.java
@@ -1,9 +1,21 @@
 package com.jk.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
 public class User {
     private Integer uid;//用户id
-    private String uername;//用户名称
+    private String username;//用户名称
     private String password;//用户登陆密码
+    private String userhead;//头像
+    private Integer usersex;//性别
+
+    private String birthday;//生日
+    private String userintro;//个人简介
+    private Integer profession;//职业
+    private Integer trade;//行业
 
     public Integer getUid() {
         return uid;
@@ -14,11 +26,11 @@ public class User {
     }
 
     public String getUername() {
-        return uername;
+        return username;
     }
 
     public void setUername(String uername) {
-        this.uername = uername;
+        this.username = uername;
     }
 
     public String getPassword() {
@@ -27,5 +39,53 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getUserhead() {
+        return userhead;
+    }
+
+    public void setUserhead(String userhead) {
+        this.userhead = userhead;
+    }
+
+    public Integer getUsersex() {
+        return usersex;
+    }
+
+    public void setUsersex(Integer usersex) {
+        this.usersex = usersex;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getUserintro() {
+        return userintro;
+    }
+
+    public void setUserintro(String userintro) {
+        this.userintro = userintro;
+    }
+
+    public Integer getProfession() {
+        return profession;
+    }
+
+    public void setProfession(Integer profession) {
+        this.profession = profession;
+    }
+
+    public Integer getTrade() {
+        return trade;
+    }
+
+    public void setTrade(Integer trade) {
+        this.trade = trade;
     }
 }
