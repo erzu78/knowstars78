cb123a653786c08b9c72c8d25c65d05ca1888b7a
zhaoying2001 <1577915681@qq.com>
2019-07-11 23:45:49 +0800

    登陆前台页面完善

--- login_consumer/src/main/java/com/jk/controller/LoginController.java
+++ login_consumer/src/main/java/com/jk/controller/LoginController.java
@@ -4,13 +4,12 @@ import com.jk.cliservice.CliService;
 import com.jk.pojo.Staff;
 import com.jk.pojo.User;
 import com.jk.service.LoginService;
+import com.jk.util.HttpClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class LoginController {
@@ -76,6 +75,30 @@ public class LoginController {
     public HashMap<String,Object> findStaff(@RequestParam("start") Integer start, @RequestParam("pageSize") Integer pageSize){
         HashMap<String,Object> hashMap=loginService.findStaff(start,pageSize);
         return hashMap;
+    }
+
+    /**
+     *手机验证码
+     */
+    @RequestMapping("sendPhoneCode")
+    @ResponseBody
+    public String sendPhoneCode(String phoneName){
+        String flg=null;//1 发送成功  2发送失败
+
+        int newcode = (int)(Math.random()*899999)+100000;
+        Map<String, Object> hashMap = new HashMap<>();
+        hashMap.put("mobile",phoneName);
+        hashMap.put("tpl_id","172072");
+        hashMap.put("tpl_value","%23code%23%3D654654"+newcode);
+        hashMap.put("key","5528967c9bf6b1568e5385379e754630");
+        try {
+            String s = HttpClient.sendGet("http://v.juhe.cn/sms/send", hashMap);
+            flg="1";
+        } catch (Exception e) {
+            e.printStackTrace();
+            flg="2";
+        }
+        return flg;
     }
 
 }
