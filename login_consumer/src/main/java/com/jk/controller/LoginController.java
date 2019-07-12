7a7a3c6fadb44c2ef0f824251c642d04f8b88997
zhaoying2001 <1577915681@qq.com>
2019-07-12 23:16:53 +0800

    fdf

--- login_consumer/src/main/java/com/jk/controller/LoginController.java
+++ login_consumer/src/main/java/com/jk/controller/LoginController.java
@@ -55,8 +55,8 @@ public class LoginController {
    //员工登录页面
     @RequestMapping(value="loginstaff")
     public String loginstaff(Staff loginstaff){
-        User userModel=loginService.loginstaff(loginstaff);
-        if(userModel!=null){
+        Staff staffModel=loginService.loginstaff(loginstaff);
+        if(staffModel!=null){
             return "1";
         }else{
             return "2";
@@ -82,23 +82,27 @@ public class LoginController {
      */
     @RequestMapping("sendPhoneCode")
     @ResponseBody
-    public String sendPhoneCode(String phoneName){
+    public Map<String, Object> sendPhoneCode(String phoneName){
         String flg=null;//1 发送成功  2发送失败
+
 
         int newcode = (int)(Math.random()*899999)+100000;
         Map<String, Object> hashMap = new HashMap<>();
         hashMap.put("mobile",phoneName);
-        hashMap.put("tpl_id","172072");
-        hashMap.put("tpl_value","%23code%23%3D654654"+newcode);
+        hashMap.put("tpl_id","172497");
+        hashMap.put("tpl_value","%23code%23%3D"+newcode);
         hashMap.put("key","5528967c9bf6b1568e5385379e754630");
+
         try {
             String s = HttpClient.sendGet("http://v.juhe.cn/sms/send", hashMap);
             flg="1";
+            hashMap.put("flg",1);
         } catch (Exception e) {
             e.printStackTrace();
-            flg="2";
+            hashMap.put("flg",2);
         }
-        return flg;
+        hashMap.put("newcode",newcode);
+        return hashMap;
     }
 
 }
