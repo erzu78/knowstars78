ac814cf49cbf2cb10cdf520a7e5712e7d1170d6a
gjgih12 <474526043@qq.com>
2019-07-12 19:47:16 +0800

    点赞 收藏

--- likecomment_consumer/src/main/java/com/jk/controller/LikeController.java
+++ likecomment_consumer/src/main/java/com/jk/controller/LikeController.java
@@ -16,73 +16,104 @@ import java.util.concurrent.TimeUnit;
 
 @RestController
 public class LikeController {
-    @Autowired
-    private LikeServiceFeign likeServiceFeign;
+   /* @Autowired
+    private LikeServiceFeign likeServiceFeign;*/
+
+
     @Autowired
     private RedisTemplate redisTemplate;
+
+
+
     /**
-     *
-     * @param key
-     * @param liveTime
+     * 操作点赞
+     * @param infId
      * @return
      */
     @RequestMapping("zizi")
-    public Long incr(String infId, long liveTime) {
-        RedisAtomicLong entityIdCounter = new RedisAtomicLong("infzhan"+infId, redisTemplate.getConnectionFactory());
-        Long increment = entityIdCounter.getAndIncrement();
+    public void incr(String infId) {
 
-        if ((null == increment || increment.longValue() == 0) && liveTime > 0) {//初始设置过期时间
-            entityIdCounter.expire(liveTime, TimeUnit.SECONDS);
+        Integer userId = 25;
+        String inId = (String) redisTemplate.opsForValue().get("userZhan"+userId+infId);
+
+        if(inId!=null){
+
+                redisTemplate.boundValueOps("infzhan"+infId).increment(-1);
+
+                redisTemplate.delete("userZhan"+userId+infId);
+
+        } else{
+            RedisAtomicLong entityIdCounter = new RedisAtomicLong("infzhan"+infId, redisTemplate.getConnectionFactory());
+            entityIdCounter.getAndIncrement();
+            redisTemplate.opsForValue().set("userZhan"+userId+infId,infId);
         }
 
-        return increment;
     }
-    @RequestMapping("zizeng")
-    public Long incrzi(String heartId, long liveTime) {
-        RedisAtomicLong redisAtomicLong = new RedisAtomicLong("infzhan"+heartId, redisTemplate.getConnectionFactory());
-        Long incrt = redisAtomicLong.getAndIncrement();
 
-        if ((null == incrt || incrt.longValue() == 0) && liveTime > 0) {//初始设置过期时间
-            redisAtomicLong.expire(liveTime, TimeUnit.SECONDS);
-        }
-
-        return incrt;
-    }
-/**
-     * @Description: 初始化自增长值
-     * @param key key
-     * @param liveTime 当前值
-    */
-   /* @RequestMapping("zeng")
-    public void setIncr(String infId, long liveTime) {
-              RedisAtomicLong counter = new RedisAtomicLong("infzhan"+infId, redisTemplate.getConnectionFactory());
-              counter.set(liveTime);
-              counter.expire(0, TimeUnit.SECONDS);
-}
-
-    @RequestMapping("zengzeng")
-    public void setIncrzeng(String heartId, long liveTime) {
-        RedisAtomicLong counter = new RedisAtomicLong("infzhan"+heartId, redisTemplate.getConnectionFactory());
-        counter.set(liveTime);
-        counter.expire(0, TimeUnit.SECONDS);
-    }
-*/
-
-
-
-
+    /**
+     * 查询点赞
+     * @param infId
+     * @return
+     */
     @GetMapping("queryZhan")
-    public String queryZhan(String infId){
+    public HashMap<String,String> queryZhan(String infId){
+
+        Integer userId = 25;
 
         String zhanCount = (String) redisTemplate.opsForValue().get("infzhan"+infId);
+        String inId = (String) redisTemplate.opsForValue().get("userZhan"+userId+infId);
 
-        return zhanCount;
+        HashMap<String,String> map = new HashMap<>();
+        map.put("zhanCount",zhanCount);
+        map.put("inId",inId);
+
+        return map;
     }
-    @GetMapping("queryshou")
-    public String queryshou(String heartId){
 
-        String shouCount = (String) redisTemplate.opsForValue().get("infzhan"+heartId);
 
-        return shouCount;
+    /**
+     * 操作收藏
+     * @param infId
+     */
+    @RequestMapping("opShou")
+    public void opShou(String infId) {
+
+        Integer userId = 25;
+        String inId = (String) redisTemplate.opsForValue().get("userShou"+userId+infId);
+
+        if(inId!=null){
+
+            redisTemplate.boundValueOps("infshou"+infId).increment(-1);
+
+            redisTemplate.delete("userShou"+userId+infId);
+
+        } else{
+            RedisAtomicLong entityIdCounter = new RedisAtomicLong("infshou"+infId, redisTemplate.getConnectionFactory());
+            entityIdCounter.getAndIncrement();
+            redisTemplate.opsForValue().set("userShou"+userId+infId,infId);
+        }
+
     }
+
+    /**
+     * 查询收藏数量
+     * @param infId
+     * @return
+     */
+    @GetMapping("queryShou")
+    public HashMap<String,String> queryShou(String infId){
+
+        Integer userId = 25;
+
+        String shouCount = (String) redisTemplate.opsForValue().get("infshou"+infId);
+        String inId = (String) redisTemplate.opsForValue().get("userShou"+userId+infId);
+
+        HashMap<String,String> map = new HashMap<>();
+        map.put("shouCount",shouCount);
+        map.put("inId",inId);
+
+        return map;
+    }
+
+
 }
