e0b5c3751aad28f37e170b86bd0ef11168747c01
zhaoying2001 <1577915681@qq.com>
2019-07-21 21:54:02 +0800

    审核，注册加头像

--- knowstars_common/src/main/java/com/jk/pojo/Staff.java
+++ knowstars_common/src/main/java/com/jk/pojo/Staff.java
@@ -5,12 +5,22 @@ public class Staff {
     private Integer sid;
     private String staffname;//登录名
     private String staffpassword;//登录密码
-    private Integer role;//员工角色 1.会员2.管理员
+    private Integer role;//角色 1.作者2.管理员
     private String staffimg;//员工头像
 
     private String staffinfo;//员工格言
 
-    private String stafftype;
+    private String stafftype;//作者等级
+
+    private Integer staffstatus;//审核状态 0.未通过1.已通过
+
+    public Integer getStaffstatus() {
+        return staffstatus;
+    }
+
+    public void setStaffstatus(Integer staffstatus) {
+        this.staffstatus = staffstatus;
+    }
 
     public String getStafftype() {
         return stafftype;
--- login_provider/src/main/java/com/jk/mapper/LoginMapper.java
+++ login_provider/src/main/java/com/jk/mapper/LoginMapper.java
@@ -26,7 +26,7 @@ public interface LoginMapper {
 
     List<User> findUser(Integer start, Integer pageSize);
 
-    @Select("select count(*) from t_user ")
+    @Select("select count(*) from t_user")
     Long UserSum();
 
     @Delete("delete  from t_user where uid=#{uid}")
@@ -38,17 +38,34 @@ public interface LoginMapper {
     @Select("select count(*) from t_staff where staffname=#{staffname}")
     Integer findStaffByName(String staffname);
 
-    @Insert("insert into t_staff(staffname,staffpassword,role) values(#{staffname},#{staffpassword},0)")
+    @Insert("insert into t_staff(staffname,staffpassword,role,staffstatus) values(#{staffname},#{staffpassword},0,0)")
     void addLoginStaff(Staff loginstaff);
 
-    List<User> findStaff(Integer start, Integer pageSize);
+    List<Staff> findStaff(Integer start, Integer pageSize);
 
-    @Select("select count(*) from t_staff ")
+    List<Staff> findStaff2(Integer start, Integer pageSize);
+
+    @Select("select count(*) from t_staff where staffstatus=0")
     Long StaffSum();
+
+    @Select("select count(*) from t_staff where staffstatus=1")
+    Long StaffSum2();
 
     @Select("select uid from t_user where username=#{username}")
     User finduserByUserName(String username);
 
     @Update("update t_staff set role=1 where sid=#{sid}")
     void updaterole(String sid);
+
+    @Update("update t_staff set staffstatus=0 where sid=#{sid}")
+    void yeshenhe(String sid);
+
+    @Update("delete from  t_staff where sid=#{sid}")
+    void deleteStaffOne(String sid);
+
+    @Select("select * from t_staff where staffname=#{staffname}")
+    Staff findStaffById(String staffname);
+
+    @Update("update  t_staff set staffimg=#{staffimg},staffinfo=#{staffinfo},stafftype=#{stafftype} where sid=#{sid}")
+    void updateStaff(Staff staff);
 }
