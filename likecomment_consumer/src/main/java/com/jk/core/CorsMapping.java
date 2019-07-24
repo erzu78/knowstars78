04dbf6821cb147770bf036d016b153dda4f0385b
gjgih12 <474526043@qq.com>
2019-07-24 16:17:21 +0800

    Merge branch 'cl' of https://github.com/erzu78/knowstars78
    # Conflicts:
    #	knowstars_web_ui/src/main/resources/templates/NewsFlash/NewsFlashAll.html

--- likecomment_consumer/src/main/java/com/jk/core/CorsMapping.java
+++ likecomment_consumer/src/main/java/com/jk/core/CorsMapping.java
@@ -11,7 +11,7 @@ public class CorsMapping implements WebMvcConfigurer {
         //addMapping 声明那些路径需要跨域访问
         registry.addMapping("/**")
                 //声明对某个服务器开启跨域
-                .allowedOrigins("http://localhost:8280","http://192.168.1.118:8280")
+                .allowedOrigins("http://localhost:8280","http://192.168.1.123:8280")
                 //设置允许跨域请求的http类型OPTIONS必须要有
         .allowedMethods("GET","POST","DELETE","PUT","OPTIONS")
         .allowCredentials(true)
